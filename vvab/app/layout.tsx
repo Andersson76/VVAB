09cf6e218f8ffe5ced961250c10d68412c5c90d6
Martin Andersson <martin.andersson@me.com>
2024-07-28 18:16:59 +0200

    Base structure

--- vvab/app/layout.tsx
+++ vvab/app/layout.tsx
@@ -1,22 +1,25 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { AppProvider } from '../context/AppContext'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Väst Ventilation AB",
-  description: "Väst Ventilation AB - Vi är proffs på ventilation",
-};
+  title: 'Väst Ventilation AB',
+  description: 'Väst Ventilation AB - Vi är proffs på ventilation',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AppProvider>{children}</AppProvider>
+      </body>
     </html>
-  );
+  )
 }
