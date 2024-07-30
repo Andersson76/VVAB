4288e91926d51f9628896f7d31a01040fd3dbc76
Martin Andersson <martin.andersson@me.com>
2024-07-30 22:49:28 +0200

    Added buttons on hero for navigation

--- vvab/components/Hero.tsx
+++ vvab/components/Hero.tsx
@@ -17,10 +17,18 @@ export default function Hero() {
       className="mt-60 flex flex-col space-y-8 items-center justify-center
     text-center overflow-hidden"
     >
-      <h1 className="text-3xl lg:text-4xl font-semibold px-10">
+      <h1 className="text-3xl lg:text-4xl font-semibold px-10 text-white-500">
         <span className="mr-3">{text}</span>
         <Cursor cursorColor="#F7AB0A" />
       </h1>
+      <div className="pt-5">
+        <Link href="#about">
+          <button className="heroButton">Om oss</button>
+        </Link>
+        <Link href="#contact">
+          <button className="heroButton lg:ml-32 ml-8 mt-40">Kontakta oss</button>
+        </Link>
+      </div>
     </div>
   )
 }
