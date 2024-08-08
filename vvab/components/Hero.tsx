2060eac78a545283cf73c5ecf8bec165ec48a04b
Martin Andersson <martin.andersson@me.com>
2024-08-08 23:22:58 +0200

    Rearranged postion of buttons

--- vvab/components/Hero.tsx
+++ vvab/components/Hero.tsx
@@ -14,22 +14,22 @@ export default function Hero() {
   })
   return (
     <div
-      className="h-screen flex relative flex-col text-center md:text-left md:flex-row
+      className="h-screen flex flex-col text-center md:text-left md:flex-row
     max-w-7xl px-10 justify-center mx-auto items-center"
     >
-      <div className='mt-40 mb-32'>
-        <h1 className="text-3xl lg:text-4xl font-semibold px-10 text-gray-200">
+      <div className="mt-60 mb-32 flex flex-col items-center">
+        <h1 className="text-3xl lg:text-4xl font-semibold px-10 text-gray-200 min-h-[60px]">
           <span className="mr-3">{text}</span>
           <Cursor cursorColor="#F7AB0A" />
         </h1>
-      </div>
-      <div>
-        <Link href="#contact">
-          <button className="heroButton ">Kontakta oss</button>
-        </Link>
-        <Link href="#about">
-          <button className="heroButton lg:ml-32 ml-8 mt-40">Om oss</button>
-        </Link>
+        <div className="mt-40 flex space-x-8">
+          <Link href="#contact">
+            <button className="heroButton mb-4">Kontakta oss</button>
+          </Link>
+          <Link href="#about">
+            <button className="heroButton">Om oss</button>
+          </Link>
+        </div>
       </div>
     </div>
   )
